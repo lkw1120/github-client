02d73edb0d499e19ecf046d13335f32d56960171
lkw1120 <lkwkang@gmail.com>
2024-08-15 18:05:43 +0900

    Feat #11 access token is expired
    Regenerate GITHUB_ACCESS_TOKEN
    Change GITHUB_ACCESS_TOKEN to read from local.properties

--- app/src/main/java/com/lkw1120/client/common/Constants.kt
+++ app/src/main/java/com/lkw1120/client/common/Constants.kt
@@ -1,9 +1,11 @@
 package com.lkw1120.client.common
 
+import com.lkw1120.client.BuildConfig
+
 object Constants {
     const val BASE_URL = "https://api.github.com/"
 
-    const val GITHUB_ACCESS_TOKEN = "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"
+    const val GITHUB_ACCESS_TOKEN = BuildConfig.GITHUB_ACCESS_TOKEN
 
     const val PAGE_SIZE = 30
 }
